5dae876ae4bfe5a05d9dd1629467588a987708ab
Pranab Mandal <pranavmandal5372@gmail.com>
2023-10-20 22:30:30 +0530

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+console.log("This program adds three numbers...")
+
+function addThreeNumbers(a, b, c) {
+  return a + b + c;
+}
+
+let c = addThreeNumbers(19, 54, 15)
+console.log(c)
