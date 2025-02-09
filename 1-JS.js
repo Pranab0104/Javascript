1e063d223f20a9cfd552cb4b24cc082b7162401c
Pranab Mandal <pranavmandal5372@gmail.com>
2025-02-09 19:53:41 +0530

    Add files via upload

---
+++ 1-JS.js
@@ -0,0 +1,25 @@
+// console.log
+console.log("Pranav Mandal")
+
+// console.warn
+console.warn("Pranav Mandal")
+
+// console.error
+console.error("Pranav Mandal")
+
+// alert("Welcome");
+
+prompt("Hiii...!");
+
+// difference of var & const.
+
+var a = 01;
+a= 26;
+var b = 10;
+b= 06;
+
+const c = 27;
+// c= 12; we never change the value once we are declared in the variable of const.
+
+console.log(a,b,c);
+
