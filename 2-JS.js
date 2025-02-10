7a8ea5a58c10783ce4849bf27510a46c76190c0f
Pranab Mandal <pranavmandal5372@gmail.com>
2025-02-10 22:43:08 +0530

    Add files via upload

---
+++ 2-JS.js
@@ -0,0 +1,32 @@
+// for loop structure
+// for(start; end; change){
+
+// }
+
+1-9
+for(var a = 1; a<10; a++){
+    console.log(a);
+}
+
+// 20-24
+for(var a = 20; a<25; a++){
+    console.log(a);
+}
+
+// 20-11
+for(var a = 20; a>10; a--){
+    console.log(a);
+}
+
+// while loop structure
+var i = 1;
+while(i<11){
+    console.log(i);
+    i++;
+}
+
+var i = 21;
+while(i>11){
+    console.log(i);
+    i--;
+}
