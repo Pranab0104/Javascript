83cb826f19da6a19b7477758f7ce038bbecfc656
Pranab Mandal <pranavmandal5372@gmail.com>
2025-02-11 23:37:37 +0530

    Add files via upload

---
+++ 3-JS.js
@@ -0,0 +1,24 @@
+function me(){
+
+    console.log("I am Pranav Mandal.");
+    console.log("From Chandrapur.");
+    console.log("I have to became a full-stack a web-developer.");
+    console.log("With a new technologies and hands on Projets.");
+    console.log("And some tools to be easier for the use of the product.");
+}
+
+me();
+
+function a (val){
+    console.log(12 + val);
+}
+a(10);
+
+// loop in array
+var arr = [1,5,48,67,27];
+
+arr.forEach(function(val){
+    console.log(val);
+    console.log(val+1);
+})
+        
