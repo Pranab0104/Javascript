c9e4539d960ef8a5eeb37d0a4dc61896b0d68087
Pranab Mandal <pranavmandal5372@gmail.com>
2025-02-12 23:19:33 +0530

    Add files via upload

---
+++ 4-JS.js
@@ -0,0 +1,12 @@
+// 1 type of making object
+var detail = {
+    name: " Pranav",
+    age : 20,
+    email : "pranavmondal@gmail.com",
+    contact : 6584765151
+};
+
+detail.name
+
+// 2 type of making object
+var obj2 = new Object();
