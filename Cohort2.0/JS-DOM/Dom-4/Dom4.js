a76840108b782324ea01bdeac06443f120d97d49
Pranab Mandal <pranavmandal5372@gmail.com>
2025-11-28 23:33:10 +0530

    Add files via upload

---
+++ Cohort2.0/JS-DOM/Dom-4/Dom4.js
@@ -0,0 +1,20 @@
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//   let h = document.createElement("h1");
+//   h.innerHTML = "Hello Master";
+//   console.log(h);
+// });
+
+// let btn = document.createElement("h1");
+// btn.innerHTML = "Download here!";
+// let main = document.querySelector('main');
+// main.appendChild(btn);
+
+let div = document.createElement('div');
+
+div.style.height= '100px';
+div.style.width = '100px';
+div.style.backgroundColor = 'crimson'
+
+let main = document.querySelector('main');
+main.appendChild(div)
